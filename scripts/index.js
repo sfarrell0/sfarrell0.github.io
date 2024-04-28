b831344850343d2dcdbf2ac73ce0cd298174443c
Sam <Sam@DESKTOP-43GT8DL>
2024-04-28 04:05:31 -0600

    alpha publish

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,8 @@ function skillSwitch(a) {
         for (let i=0; i< options.length; i++) {
             if (options[i].id == a){
                 options[i].hidden = false
-                console.log("show 1")
             } else {
                 options[i].hidden = true
-                console.log("hide 1")
             }
                 
         }
